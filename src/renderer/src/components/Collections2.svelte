e8bef5391c0aa40abf1a154a4eec3dfcc91ce6cc
pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-12-26 17:20:07 +0100

    add back the collections checkbox

--- src/renderer/src/components/Collections2.svelte
+++ src/renderer/src/components/Collections2.svelte
@@ -6,6 +6,7 @@
     type ModalSettings
   } from '@skeletonlabs/skeleton'
   import { Album } from 'lucide-svelte'
+  import { onlyShowModsNotInAnyCollection } from '../stores/library'
   import { collections, selectedCollectionName } from '../stores/profile'
   import CollectionsModal from './modals/CollectionsModal.svelte'
 
@@ -38,6 +39,18 @@
     <button class="btn btn-sm btn-ghost-primary" on:click={triggerCustomModal}>
       <Album size={16} />
       <span>Manage</span></button
+    >
+  </div>
+
+  <div class="mt-4 flex items-center gap-3 ">
+    <input
+      id="onlyShowModsNotInAnyCollection"
+      type="checkbox"
+      disabled={$selectedCollectionName !== ''}
+      bind:checked={$onlyShowModsNotInAnyCollection}
+    />
+    <label class="inline" for="onlyShowModsNotInAnyCollection">
+      Only show mods not in any collection</label
     >
   </div>
 </div>
